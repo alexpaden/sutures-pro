b99229024f7b73c147349dd910ad7ed6fed46237
Alex Paden <padenfool@gmail.com>
2023-02-05 17:38:01 -0600

    add some notes

--- src/pages/api/translate.ts
+++ src/pages/api/translate.ts
@@ -1,5 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+// options for the medical report
+// choose from training set type
+// choose length of response (short, medium, long)
+// choose multiple choice
+// choose to create dialogue with bot
+
 export default (req: NextApiRequest, res: NextApiResponse) => {
   const medicalReport = req.body.medicalReport;
   res.status(200).json(medicalReport + "!");
