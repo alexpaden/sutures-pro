20f3ae7b67f82fa0892b06b9f814488a72ef640f
Alex Paden <padenfool@gmail.com>
2023-02-06 09:29:44 -0600

    openai translate api

--- src/pages/api/translate.ts
+++ src/pages/api/translate.ts
@@ -1,12 +1,27 @@
 import { NextApiRequest, NextApiResponse } from "next";
+const { Configuration, OpenAIApi } = require("openai");
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const medicalReport = req.body.medicalReport;
+
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "Hello world",
+  });
+
+  console.log(completion.data.choices[0].text);
+
+  res.status(200).json(medicalReport + "!");
+};
 
 // options for the medical report
 // choose from training set type
 // choose length of response (short, medium, long)
 // choose multiple choice
 // choose to create dialogue with bot
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  const medicalReport = req.body.medicalReport;
-  res.status(200).json(medicalReport + "!");
-};
